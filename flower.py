9ead961e8c8e2a88f722df10ef3790318563c1a5
Emangbol <162456951+Emangbol@users.noreply.github.com>
2024-03-06 20:18:06 +0700

    Create flower.py

---
+++ flower.py
@@ -0,0 +1,12 @@
+-import turtle
+- t = turtle.Turtle()
+- s = turtle.Screen()
+- s.bgcolor("black")
+- t.pencolor("pink")
+- 
+- ⚡ Fun fact: ...
+
+<!---
+Emangbol/Emangbol is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
